61338c28eff88e076c018ff43ae0439b1c6e427d
Laurent Eschenauer <laurent@eschenauer.be>
2013-05-20 12:29:16 +0200

    Integrated initial version of the dronestream based video plugin

---
+++ plugins/video-stream/index.js
@@ -0,0 +1,5 @@
+function video(name, deps) {
+    require("dronestream").listen(3001);
+};
+
+module.exports = video;
---
+++ plugins/video-stream/public/js/index.js
@@ -0,0 +1,15 @@
+(function(window, document) {
+
+        var Video = function Video(cockpit) {
+            console.log("Initializing video stream plugin.");
+            var video = this;
+            
+            // Add some UI elements
+            $('#cockpit').append('<div id="dronestream"></div>');
+
+            // Start the stream
+            new NodecopterStream(document.getElementById("dronestream"), {port: 3001});
+	};
+
+	window.Cockpit.plugins.push(Video);
+}(window, document));
